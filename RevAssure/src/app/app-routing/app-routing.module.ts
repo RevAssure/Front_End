dabb764b23a70763ddedb68f312249e08dfa39c9
Alex Nowak <anowak2692@gmail.com>
2021-09-17 10:54:14 -0500

    Added skeleton components for Modules, Topic, CreateModule, and CreateTopic. Added a navbar dropdown for logging out.

--- RevAssure/src/app/app-routing/app-routing.module.ts
+++ RevAssure/src/app/app-routing/app-routing.module.ts
@@ -5,13 +5,23 @@ import { RegisterComponent } from '../components/register/register.component';
 import { Routes, RouterModule } from '@angular/router';
 import { CurriculumComponent } from '../components/curriculum/curriculum.component';
 import { DayViewComponent } from '../components/day-view/day-view.component';
+import { DashboardComponent } from '../components/dashboard/dashboard.component';
+import { ModulesComponent } from '../components/modules/modules.component';
+import { CreateModuleComponent } from '../components/create-module/create-module.component';
+import { CreateTopicComponent } from '../components/create-topic/create-topic.component';
+import { TopicComponent } from '../components/topic/topic.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'curriculum', component: CurriculumComponent},
-  { path: 'curriculum/:date', component: DayViewComponent}
+  { path: 'curriculum/:date', component: DayViewComponent},
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'modules', component: ModulesComponent},
+  { path: 'createModule', component: CreateModuleComponent},
+  { path: 'createTopic', component: CreateTopicComponent},
+  { path: 'topic', component: TopicComponent} //TODO: add :id param
 ]
 
 @NgModule({
