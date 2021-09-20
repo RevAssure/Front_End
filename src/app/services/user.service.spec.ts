75646556dd6df8a5d9bf5816ec3985b6ccb63826
Jared Mullins <jwmullins92@gmail.com>
2021-09-20 15:07:44 -0500

    login and register making successful back end calls

---
+++ src/app/services/user.service.spec.ts
@@ -0,0 +1,24 @@
+import { TestBed } from '@angular/core/testing';
+
+import { UserService } from './user.service';
+
+describe('UserService', () => {
+  let service: UserService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(UserService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should expect a user', async () => {
+    // service.registerUser().subscribe(result => expect(result).toBe);
+    
+  })
+});
+
+
+
