ed9f33e48f4e77a106f2e84a3000cf37ff7261c4
Minh Thuy Nguyen <nguyentranminhthuy96@gmail.com>
2021-09-21 08:42:33 -0700

    Added documentation + strict method typing to user service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -12,11 +12,22 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
+  /**
+   * Sends a POST request to "/revuser/register" to register a new user
+   * @param newUser User object containing Username, Password, First Name and Last Name of user to be registered
+   * @returns Observable of persisted User object if successful.
+   */
   registerNewUser(newUser: User): Observable<User> {
     return this.http.post<User>("http://localhost:8081/revuser/register", newUser)
     
   }
 
+  /**
+   * Sends a POST request to "revuser/authenticate" to attempt to login.
+   * @param username Username of user
+   * @param password Password of user
+   * @returns Observable of an object containing the JWT if successful.
+   */
   login(username: string, password: string): Observable<Jwt> {
     const authObject = {
       username,
