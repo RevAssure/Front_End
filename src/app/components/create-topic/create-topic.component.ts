f881c302d2487d491ff67329fbd2983412b49e50
Jintao Lin <jtlin226@gmail.com>
2021-09-26 15:48:11 -0500

    Fixed: Set default module when add new topic in module

--- src/app/components/create-topic/create-topic.component.ts
+++ src/app/components/create-topic/create-topic.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { TechnologyCategory } from 'src/app/technologycategory';
 import { UserService } from 'src/app/services/user.service';
 import { TechCategoryService } from 'src/app/services/tech-category.service';
@@ -6,8 +6,9 @@ import { TopicService } from 'src/app/services/topic.service';
 import { AuthorizationService } from 'src/app/services/authorization.service';
 import { Module } from 'src/app/module';
 import { ModuleService } from 'src/app/services/module.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Topic } from 'src/app/topic';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-create-topic',
@@ -18,7 +19,7 @@ export class CreateTopicComponent implements OnInit {
 
   constructor(private userService: UserService, private techCategoryService: TechCategoryService,
     private topicService: TopicService, private authService: AuthorizationService,
-    private moduleService: ModuleService, private router: Router) { }
+    private moduleService: ModuleService, private location: Location, private route: ActivatedRoute) { }
 
   title: string =  '';
   description: string =  '';
@@ -26,7 +27,7 @@ export class CreateTopicComponent implements OnInit {
   lectureNotes: string = '';
   githubRepo: string =  '';
   technologyCategoryId: string = "1";
-  moduleId: string = "1";
+  moduleId: string = "";
 
   successful: boolean = false;
 
@@ -36,7 +37,13 @@ export class CreateTopicComponent implements OnInit {
   ngOnInit() {
     this.techCategories = this.techCategoryService.categories;
     this.moduleService.getAllModules(this.authService.jwt).subscribe(modules => this.modules = modules);
+    let moduleId = this.route.snapshot.queryParamMap.get('moduleId')
+    console.log(moduleId)
+    if(moduleId) {
+      this.moduleId = `${moduleId}`
+    }
   }
+  
 
   /**
    * Create a new topic. The newTopicPostBody represents the TopicDto that will be
@@ -52,14 +59,18 @@ export class CreateTopicComponent implements OnInit {
       githubRepo: this.githubRepo,
       trainer: this.userService.getUserId(),
       technologyCategory: Number.parseInt(this.technologyCategoryId),
-      modules: [Number.parseInt(this.moduleId)]
+      modules: [0]
+    }
+    newTopic.modules.pop();
+    if(this.moduleId) {
+      newTopic.modules.push(Number.parseInt(this.moduleId))
     }
     console.log(newTopic);
     this.topicService.createTopicWithDto(this.authService.jwt, newTopic).subscribe( (result) => {
       console.log(result);
       this.successful = true;
       setTimeout(() => {
-        this.router.navigateByUrl("/modules");
+        this.location.back();
       }, 3000);
     });
   }
