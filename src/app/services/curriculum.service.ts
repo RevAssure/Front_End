927f6499788c426e0e1dd3d2ec34448d11415ada
Minh Thuy Nguyen <nguyentranminhthuy96@gmail.com>
2021-09-27 16:41:37 -0700

    Added/updated documentation for services/adapt functions

--- src/app/services/curriculum.service.ts
+++ src/app/services/curriculum.service.ts
@@ -31,6 +31,13 @@ export class CurriculumService {
 
   private eventUpdate = new Subject<any>(); 
 
+  /**
+   * Performs a POST to "/curriculum" to register a new curriculum to database.
+   * All fields are initialized to appropriate falsy values except for title (uses function param as title)
+   * and trainer (uses currently logged in user).
+   * @param newTitle - (string) Title of new Curriculum
+   * @returns - an Observable of new Curriculum as it is persisted in database.
+   */
   createCurriculum(newTitle: string){
     let newCurriculum: Curriculum = {
       id: 0,
@@ -41,16 +48,19 @@ export class CurriculumService {
     }
     this.httpOptions.headers = this.httpOptions.headers.set('Authorization', `Bearer ${this.authService.jwt}`);
     return this.http.post<Curriculum>(this.url, newCurriculum, this.httpOptions)
-
   }
 
+  /**
+   * Converts an object representing an Event to a CalendarEvent object that is compatible with FullCalendar API.
+   * @param e - Object to be converted
+   * @param topics - (Topic[]) Array of topics that should contain the Topic the Event is derived from.
+   * @returns - Converted CalendarEvent.
+   */
   convertToCalendarEvent(e: any, topics: Topic[]) {
     let foundTopic = topics.filter(t => {
-      console.log(t.id + " " + e.id)
       return t.id == e.topic.id
     })
     let title = foundTopic[0].title;
-    console.log(title)
     let date = new Date(e.startDatetime * 1000)
     let year = date.getFullYear();
     let month = (date.getMonth() + 1).toString();
@@ -72,22 +82,37 @@ export class CurriculumService {
     return calEvent
   }
 
+  /**
+   * Performs a POST to "/event" to register a new Event to database.
+   * @param event - Object representing Event to be persisted.
+   * @returns - an Observable of Object representing Event as it is persisted.
+   */
   addEvent(event: any) {
-    console.log(this.authService.jwt)
     this.httpOptions.headers = this.httpOptions.headers.set('Authorization', `Bearer ${this.authService.jwt}`);
     return this.http.post(`${environment.revAssureBase}event`, event, this.httpOptions)
   }
 
+  /**
+   * Performs a DELETE to "/event/{id}" to delete an Event with ID {id} to database.
+   * @param id - (number) ID of Event to be deleted.
+   * @returns - an Observable that completes when a response is received from endpoint.
+   */
   deleteEventById(id: number) {
     console.log(this.authService.jwt)
     this.httpOptions.headers = this.httpOptions.headers.set('Authorization', `Bearer ${this.authService.jwt}`);
     return this.http.delete(`${environment.revAssureBase}event/${id}`, this.httpOptions)
   }
 
+  /**
+   * Performs a GET depending on whether current user is a Trainer or not.
+   * If Trainer: GET "/curriculum", returns curricula owned by user.
+   * If Associate: GET "/curriculum/assigned", returns curricula assigned to user.
+   * @param isTrainer - (boolean) Denotes whether current user is a trainer or not.
+   * @returns - an Observable of the Curriculum array retrieved.
+   */
   getCurriculum(isTrainer: boolean): Observable<Curriculum[]>{
       this.httpOptions.headers = this.httpOptions.headers.set('Authorization', `Bearer ${this.authService.jwt}`);
       if(isTrainer) {
-        console.log("Trainer")
         return this.http.get<Curriculum[]>(this.url, this.httpOptions)
       } else {
         console.log("Associate")
@@ -95,6 +120,11 @@ export class CurriculumService {
       }
   }
 
+  /**
+   * Same as getCurriculum(), but always assumes the current user is an associate.
+   * Performs a GET on "/curriculum/assigned" for curricula assigned to user.
+   * @returns - an Observable of the Curriculum array retrieved.
+   */
   getCurriculumAssociate() {
     this.httpOptions.headers = this.httpOptions.headers.set('Authorization', `Bearer ${this.authService.jwt}`);
     return this.http.get<any[]>(this.associateURL, this.httpOptions)
