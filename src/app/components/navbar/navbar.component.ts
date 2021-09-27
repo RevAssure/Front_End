b52d826df652ed173c4b35b52afca8ab12941311
Jintao Lin <jtlin226@gmail.com>
2021-09-27 18:56:30 -0500

    Added documentation

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -4,6 +4,11 @@ import { AuthorizationService } from 'src/app/services/authorization.service';
 import { UserService } from 'src/app/services/user.service';
 import { User } from 'src/app/user';
 
+/**
+ * The navbar component is used to display a navigation bar at the
+ * top of the web page and will be utilized in every components
+ * of this application.
+ */
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -11,12 +16,17 @@ import { User } from 'src/app/user';
 })
 export class NavbarComponent implements OnInit {
 
+  // Injected AuthorizationService and UserService to bring in methods to grab needed information
   constructor(private authService: AuthorizationService, private userService: UserService) { }
 
+  // A boolean determines if user logged in is trainer or associate
   trainer: boolean;
+  // the username of user logged in
   username: string;
+  // the firstname of user logged in
   firstName: string;
 
+  // Set properties when on initiation by calling the service methods
   ngOnInit(): void {
     this.trainer = this.userService.isTrainer();
     this.username = this.userService.getUsername();
