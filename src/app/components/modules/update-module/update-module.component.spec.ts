af8f885935ab33f85620a0f17a33188d75edef60
Alex Nowak <anowak2692@gmail.com>
2021-09-27 19:47:32 -0500

    Changes to UpdateModules spec test and ModuleListItem spec test to pass component init tests.

--- src/app/components/modules/update-module/update-module.component.spec.ts
+++ src/app/components/modules/update-module/update-module.component.spec.ts
@@ -1,20 +1,31 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { Component, DebugElement } from '@angular/core';
 
 import { UpdateModuleComponent } from './update-module.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NavbarComponent } from '../../navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-navbar',
+  template: ''
+})
+class FakeNavbarComponent implements Partial<NavbarComponent>{}
 
 describe('UpdateModuleComponent', () => {
   let component: UpdateModuleComponent;
   let fixture: ComponentFixture<UpdateModuleComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(async() => {
     TestBed.configureTestingModule({
-      declarations: [ UpdateModuleComponent ]
+      declarations: [ UpdateModuleComponent, FakeNavbarComponent],
+      imports: [HttpClientTestingModule, RouterTestingModule, FormsModule]
     })
     .compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(UpdateModuleComponent);
